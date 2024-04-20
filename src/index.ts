04fce2e6a73944f84b5421fe0327bce8968c8cdd
Shin'ya Ueoka <ueokande@i-beam.org>
2024-04-20 04:41:36 +0000

    export as single package

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import * as bootstrap from "./bootstrap/index.js";
+import * as pkg from "./pkg/index.js";
+
+export { pkg, bootstrap };
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: {
-    pkg: 'src/pkg/index.ts',
-    bootstrap: 'src/bootstrap/index.ts'
-  },
+  entry: ['src/index.ts'],
   format: ['esm'],
   dts: true,
   splitting: false,
