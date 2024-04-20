ae5a5ec69f0da1f64fb2af3ac853284527d25390
Shin'ya Ueoka <ueokande@i-beam.org>
2024-04-20 07:02:34 +0000

    add bootstrap package

--- src/bootstrap/index.ts
+++ src/bootstrap/index.ts
@@ -1,5 +1,22 @@
-const run = () => {
-  return 42;
+import * as core from "@actions/core";
+
+const hasErrorMessage = (e: unknown): e is { message: string | Error } => {
+  return typeof e === "object" && e !== null && "message" in e;
+};
+
+const run = async (fn: () => unknown | Promise<unknown>): Promise<void> => {
+  try {
+    await fn();
+  } catch (e) {
+    process.stderr.write("Error: ");
+    if (hasErrorMessage(e)) {
+      core.setFailed(e.message);
+    } else if (e instanceof Error) {
+      core.setFailed(e.message);
+    } else {
+      core.setFailed(String(e));
+    }
+  }
 };
 
 export { run };
--- test/bootstrap/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from "vitest";
-import { run } from "../../src/bootstrap/index";
-
-describe("bootstrap", () => {
-  test("run", async () => {
-    const result = run();
-    expect(result).toBe(42);
-  });
-});
